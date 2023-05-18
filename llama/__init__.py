b2415e003fb8d67efccb3b190925682de242223e
tpoisonooo <khj.application@aliyun.com>
2023-05-18 10:40:01 +0000

    fix(llama/__init__.py): fix missing import

--- llama/__init__.py
+++ llama/__init__.py
@@ -2,4 +2,4 @@ from .tokenizer import Tokenizer
 from .decoder import Decoder
 from .utils import npsoftmax, npmultinominal2D
 from .logits_process import warp_temperature, warp_topk, sample_logits
-from .memory_pool import MemoryPoolSimple
+from .memory_pool import MemoryPoolSimple, OrtWrapper
